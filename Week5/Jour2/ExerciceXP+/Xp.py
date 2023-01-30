e0c2fac75d78b6afa533ba449789814b635c0229
Donmontienou <koudenour@gmail.com>
2023-01-30 15:21:01 +0000

    Execice Xp+ du jour2 week 5

--- Week5/Jour2/ExerciceXP+/Xp.py
+++ Week5/Jour2/ExerciceXP+/Xp.py
@@ -1,11 +1,12 @@
 from os import system
+import random
 print("\n")
 print("\t\t\t\tExercice1: Famille introductions")
 print("\n")
 
 class Family():
 	def __init__(self,membres,nom_famille):
-		self.membres=[membres]
+		self.membres=membres
 		self.nom_famille=nom_famille
 
 	def born(self,**kwargs):
@@ -15,25 +16,25 @@ class Family():
 
 	def is_18(self,name):
 		for elem in self.membres:
-			if elem["name"]==name:
-				if elem["age"]>18:
-					return True
+			if elem['name']==name:
+				if elem['age']>18:
+					print(f"")
+					print("True") 
 				else:
-					return False
+					print("False")
+
 
 	def family_presentation(self):
 		for nom in self.membres:
-			print(f"{self.nom_famille}:{nom}")
+			print(f"{self.nom_famille}:" ,nom["name"])
 
 
-
-x={'name':'Michael','age':35,'gender':'Male','is_child':'False'}
-y={'name':'Sarah','age':32,'gender':'Female','is_child':'False'}
+x=[{'name':'Michael','age':35,'gender':'Male','is_child':'False'},{'name':'Sarah','age':32,'gender':'Female','is_child':'False'}]
 
 Famille=Family(x,'Konate')
 Famille.born(name='Gerome',age=5,gender='Male',is_child='True')
+Famille.is_18('Michael')
 Famille.family_presentation()
-print(Famille.is_18('Gerome'))
 
 system("pause")
 
@@ -43,6 +44,29 @@ print("\t\t\t\tExercice2: Famille introductions")
 print("\n")
 
 class TheIncredibles(Family):
-	def __init__(self):
-		
+	pass
 
+	def user_power(self):
+		for elem in self.membres:
+			if elem["age"]>18:
+				print(elem["power"])
+			else:
+				print("Vous n'êtes pas puissant")
+				
+
+	def incredible_presentation(self):
+		super().__init__(self.membres,self.nom_famille)
+		for nom in self.membres:	
+				print(f"{self.nom_famille}:",nom["name"])
+
+		super().family_presentation()
+
+x1=[{'name':'Michael','age':35,'gender':'Male','is_child':'False','power':'fly','incredible_name':'MikeFly'},
+{'name':'Sarah','age':32,'gender':'Female','is_child':'False','power':'read minds','incredible_name':'SuperWoman'}]
+
+
+famille=TheIncredibles(x1, 'Traore')
+famille.user_power()
+famille.incredible_presentation()
+famille.born('name':'Jack','age':0,'gender':'Male','is_child':True,'power': 'Puissance inconnue','incredible_name':'nom incredible inconnu')
+famille.incredible_presentation()
--- Week5/Jour2/ExercicesDefi/def.py
+++ Week5/Jour2/ExercicesDefi/def.py
@@ -1,14 +1,8 @@
-import random
-import pygame
-from math import infinity
-import sys
-import os
-import time
- 
- pygame.init()
- pygame.font.init()
- widht _ Hieght=3,3
- cross=pygame.transforme.scale(pygame.image.load(os.path.join("assets,"cross.png)),(Widht//3,Hieght//3)
+from os import system
+class pagintation():
+	def __init__(self,items=[],pageSize=10):
+		self.items=items
+		self.pageSize=pageSize
 
-
- 	
+	def getVisibleItems(self):
+		
